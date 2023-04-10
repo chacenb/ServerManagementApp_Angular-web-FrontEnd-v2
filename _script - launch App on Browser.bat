f57efb708ee3a4785df3061ffdf234f74e583778
chace <chacenb@gmail.com>
2023-04-10 10:16:14 +0100

    init

---
+++ _script - launch App on Browser.bat
@@ -0,0 +1,15 @@
+@echo off
+echo ########################################
+cd %CD%
+echo # Running %~nx0
+echo # LARCO - GEADAIE - Launch #########
+echo ########################################
+
+cd %CD%
+cd
+ng s -o	
+cmd /k 
+timeout /t 5 
+
+@REM pause | | 
+@REM  --port 4300
