5cc1a4c8c6888fb3b852da632096d6f0572ff1ca
chace <chacenb@gmail.com>
2023-04-10 10:52:15 +0100

    all printing options OK

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ServerManagementApp-Angular-web-FrontEnd-v2'`, () => {
+  it(`should have as title 'serverManagement'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ServerManagementApp-Angular-web-FrontEnd-v2');
+    expect(app.title).toEqual('serverManagement');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ServerManagementApp-Angular-web-FrontEnd-v2 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('serverManagement app is running!');
   });
 });
